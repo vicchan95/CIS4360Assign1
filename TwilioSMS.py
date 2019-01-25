3b1de73a58a29e0a765ff22f19fd3ea8b6feba84
vicchan95 <vicchan95@gmail.com>
2019-01-25 17:49:30 -0500

    Update TwilioSMS.py
    Copy and pasted the API client call with a text body added to the message.create() function call.

--- TwilioSMS.py
+++ TwilioSMS.py
@@ -4,15 +4,17 @@ Assignment 1 - Calling Twilio and Google APIs
 Part 1: Twilio
 """
 
-from twilio.rest import Client 
- 
-account_sid = 'ACbfc6fb54f17475ad4c94dc95c4808623' 
-auth_token = '[AuthToken]' 
-client = Client(account_sid, auth_token) 
- 
-message = client.messages.create( 
-                              from_='+12673231648',        
-                              to='+12157794163' 
-                          ) 
- 
+from twilio.rest import Client
+
+account_sid = "ACbfc6fb54f17475ad4c94dc95c4808623"
+auth_token  = "[Auth_token]"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+12157794163",
+    from_="+12673231648",
+    body="Hello from Python!")
+
 print(message.sid)
+
